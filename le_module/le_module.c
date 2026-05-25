52414fa7075a2deec67763c64fc7d880fbb79a27
Laurent Fiack <laurent.fiack@gmail.com>
2026-05-25 15:36:54 +0200

    module with timer

--- le_module/le_module.c
+++ le_module/le_module.c
@@ -3,6 +3,8 @@
 #include <linux/init.h>
 #include <linux/types.h>    // uint32_t
 
+#include <linux/timer.h>
+
 #include <linux/memblock.h>
 #include <linux/init.h>
 #include <linux/io.h>
@@ -13,13 +15,52 @@ MODULE_AUTHOR("Quelqu'un");
 MODULE_DESCRIPTION("Exemple de module");
 MODULE_LICENSE("GPL");
 
-static volatile uint32_t * p;
+#define TIMEOUT 10
+
+static volatile uint32_t * portb;
+
+static struct timer_list timer;
+
+void led_on(void);
+void led_off(void);
+void led_toggle(void);
+
+void led_on(void)
+{
+    *portb = (1<<24);
+}
+
+void led_off(void)
+{
+    *portb &= ~(1<<24);
+}
+
+void led_toggle(void)
+{
+    if (*portb & (1<<24)) {
+        led_off();
+    }
+    else {
+        led_on();
+    }
+}
+
+static void timer_handler(struct timer_list *t) {
+
+    /* Il faut réarmer le timer si l'on veut un appel périodique */
+    mod_timer(&timer,jiffies + TIMEOUT);
+
+    led_toggle();
+}
 
 static int __init le_module_init(void) {
     printk(KERN_INFO "Hello world!\n");
     
-    p = ioremap(0xff709000, 4);
-    *p = (1<<24);
+    portb = ioremap(0xff709000, 4);
+    
+    led_on();
+    timer_setup(&timer, timer_handler, 0);
+    mod_timer(&timer, jiffies + TIMEOUT);
 
     return 0;
 }
@@ -27,7 +68,8 @@ static int __init le_module_init(void) {
 static void __exit le_module_exit(void) {
     printk(KERN_ALERT "Bye bye...\n");
 
-    *p = 0;
+    timer_delete(&timer);
+    led_off();
 }
 
 module_init(le_module_init);
